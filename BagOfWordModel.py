56aa32e78bb2818e311558aca913bc67189567c7
schuetzi <g.schuetzeneder@gmail.com>
2019-06-15 20:46:40 +0200

    new files

--- BagOfWordModel.py
+++ BagOfWordModel.py
@@ -1,20 +1,90 @@
-from keras.preprocessing.text import Tokenizer
+import string
+import re
+from os import listdir
+from collections import Counter
+from nltk.corpus import stopwords
 
-# define 5 documents
-docs = ['Well done!',
-        'Good work',
-        'Great effort',
-        'nice work',
-        'Excellent!']
-# create the tokenizer
-t = Tokenizer()
-# fit the tokenizer on the documents
-t.fit_on_texts(docs)
-# summarize what was learned
-print(t.word_counts)
-print(t.document_count)
-print(t.word_index)
-print(t.word_docs)
-# integer encode documents
-encoded_docs = t.texts_to_matrix(docs, mode='count')
-print(encoded_docs)
+
+# load doc into memory
+def load_doc(filename):
+    # open the file as read only
+    file = open(filename, 'r')
+    # read all text
+    text = file.read()
+    # close the file file.close()
+    return text
+
+# turn a doc into clean tokens
+def clean_doc(doc):
+    # split into tokens by white space
+    tokens = doc.split()
+    # prepare regex for char filtering
+    re_punc = re.compile('[%s]' % re.escape(string.punctuation))
+    # remove punctuation from each word
+    tokens = [re_punc.sub('', w) for w in tokens]
+    # remove remaining tokens that are not alphabetic
+    tokens = [word for word in tokens if word.isalpha()]
+    # filter out stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if not w in stop_words]
+    # filter out short tokens
+    tokens = [word for word in tokens if len(word) > 1]
+    return tokens
+
+# load doc and add to vocab
+def add_doc_to_vocab(filename, vocab):
+    # load doc
+    doc = load_doc(filename)
+    # clean doc
+    tokens = clean_doc(doc)
+    # update counts
+    vocab.update(tokens)
+
+# load doc, clean and return line of tokens
+def doc_to_line(filename, vocab):
+    # load the doc
+    doc = load_doc(filename)
+    # clean doc
+    tokens = clean_doc(doc)
+    # filter by vocab
+    tokens = [w for w in tokens if w in vocab]
+    return ' '.join(tokens)
+
+# load all docs in a directory
+def process_docs(directory, vocab):
+    lines = list()
+    # walk through all files in the folder
+    for filename in listdir(directory):
+        # skip files that do not have the right extension
+        if not filename.endswith(".txt"):
+            next
+        # create the full path of the file to open
+        path = directory + '/' + filename
+        # load and clean the doc
+        line = doc_to_line(path, vocab)
+        # add to list
+        lines.append(line)
+    return lines
+
+# save list to file
+def save_list(lines, filename):
+    # convert lines to a single blob of text
+    data = '\n'.join(lines)
+    # open file
+    file = open(filename, 'w')
+    # write text
+    file.write(data)
+    # close file
+    file.close()
+
+# load vocabulary
+vocab_filename = 'vocab.txt'
+vocab = load_doc(vocab_filename)
+vocab = vocab.split()
+vocab = set(vocab)
+# prepare negative reviews
+negative_lines = process_docs('txt_sentoken/neg', vocab)
+save_list(negative_lines, 'negative.txt')
+# prepare positive reviews
+positive_lines = process_docs('txt_sentoken/pos', vocab)
+save_list(positive_lines, 'positive.txt')
