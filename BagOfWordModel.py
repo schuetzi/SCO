8df3cb1f8e7284a865d09eee39f3add4902cbcab
schuetzi <g.schuetzeneder@gmail.com>
2019-06-15 18:38:46 +0200

    added sentiments

--- BagOfWordModel.py
+++ BagOfWordModel.py
@@ -1,13 +1,20 @@
-from keras.preprocessing.text import one_hot, hashing_trick
-from keras.preprocessing.text import text_to_word_sequence
+from keras.preprocessing.text import Tokenizer
 
-# define the document
-text = 'The quick brown fox jumped over the lazy dog.'
-# estimate the size of the vocabulary
-words = set(text_to_word_sequence(text))
-vocab_size = len(words)
-print(vocab_size)
-# integer encode the document
-result = hashing_trick(text, round(vocab_size*1.3), hash_function='md5')
-print(result)
-
+# define 5 documents
+docs = ['Well done!',
+        'Good work',
+        'Great effort',
+        'nice work',
+        'Excellent!']
+# create the tokenizer
+t = Tokenizer()
+# fit the tokenizer on the documents
+t.fit_on_texts(docs)
+# summarize what was learned
+print(t.word_counts)
+print(t.document_count)
+print(t.word_index)
+print(t.word_docs)
+# integer encode documents
+encoded_docs = t.texts_to_matrix(docs, mode='count')
+print(encoded_docs)
